8fb94656562cf5f12e1c0c5f957e9940b8948878
fedorzvyk <fedorzvyk@gmail.com>
2023-03-06 11:17:54 +0200

    added validation to model

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -4,22 +4,22 @@ const Contact = require('../../models/contact');
 
 const addContact = async (req, res, next) => {
   try {
-    let { name, email, phone, favorite } = req.body;
+    // let { name, email, phone, favorite } = req.body;
 
-    if (!name) {
-      return res.status(400).json({ message: 'missing required name field' });
-    }
-    if (!email) {
-      return res.status(400).json({ message: 'missing required email field' });
-    }
-    if (!phone) {
-      return res.status(400).json({ message: 'missing required phone field' });
-    }
-    if (!favorite) {
-      favorite = false;
-    }
+    // if (!name) {
+    //   return res.status(400).json({ message: 'missing required name field' });
+    // }
+    // if (!email) {
+    //   return res.status(400).json({ message: 'missing required email field' });
+    // }
+    // if (!phone) {
+    //   return res.status(400).json({ message: 'missing required phone field' });
+    // }
+    // if (!favorite) {
+    //   favorite = false;
+    // }
 
-    const contact = await Contact.create({ name, email, phone, favorite });
+    const contact = await Contact.create(req.body);
 
     res.status(201).json(contact);
   } catch (error) {
