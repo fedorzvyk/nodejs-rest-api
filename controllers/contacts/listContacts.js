c4362e29603180d9c33a043e3ef4d72ef518d2c1
fedorzvyk <fedorzvyk@gmail.com>
2023-03-07 23:25:57 +0200

    added contacts filter

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -4,10 +4,11 @@ const Contact = require('../../models/contact');
 const listContacts = async (req, res, next) => {
   try {
     const { _id } = req.user;
-    const { page = 1, limit = 10 } = req.query;
+    const { page = 1, limit = 10, favorite } = req.query;
     const skip = (page - 1) * limit;
+    const filterContacts = favorite ? { favorite, owner: _id } : { owner: _id };
 
-    const contactsList = await Contact.find({ owner: _id }, '', {
+    const contactsList = await Contact.find(filterContacts, '', {
       skip,
       limit: Number(limit),
     }).populate('owner', '_id email subscription');
