8f17c14e039460c0ae6b270c63123c4a1ae41c07
fedorzvyk <fedorzvyk@gmail.com>
2023-03-07 22:26:20 +0200

    added pagination

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -4,11 +4,13 @@ const Contact = require('../../models/contact');
 const listContacts = async (req, res, next) => {
   try {
     const { _id } = req.user;
+    const { page = 1, limit = 10 } = req.query;
+    const skip = (page - 1) * limit;
 
-    const contactsList = await Contact.find({ owner: _id }).populate(
-      'owner',
-      '_id email subscription'
-    );
+    const contactsList = await Contact.find({ owner: _id }, '', {
+      skip,
+      limit: Number(limit),
+    }).populate('owner', '_id email subscription');
     res.status(200).json(contactsList);
   } catch (error) {
     next(error);
